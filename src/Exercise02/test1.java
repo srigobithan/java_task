bc49f8661a0b5ad6362c4b1b6496f835445f2235
srigobithan <gobithan96.com>
2023-01-10 18:50:13 +0530

    2nd update

--- src/Exercise02/test1.java
+++ src/Exercise02/test1.java
@@ -8,9 +8,10 @@ public class test1 {
 		int test[] = {9,8,7,3,15,7,9,4,9,2,8,3,8,8};
 		
 		int UniqueNumbers;
-		 for (int i = 0; i < UniqueNumbers.size(); ++i) {
+		  for (int i = 0; i < UniqueNumbers.size(); ++i) {
 	            System.out.println(UniqueNumbers.get(i));
 	        }
+		  gobi
 	}
 
 }
